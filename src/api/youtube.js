6dead323c9d0ff0f59872a48e0ec42cdf1bed615
prymskiwen <hello.pronim@gmail.com>
2022-12-14 10:38:47 +1000

    feat: register youtube api key to .env

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const KEY = "AIzaSyDH5JRIpZg_owk1Ff6m2sA4_pJFqU6vQTg";
+// const KEY = "AIzaSyDH5JRIpZg_owk1Ff6m2sA4_pJFqU6vQTg";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
   params: {
     part: "snippet",
     maxResults: 5,
-    key: KEY,
+    key: process.env.REACT_APP_YOUTUBE_API_KEY,
   },
 });
